53c37ce425e9e870a91ff75bfc43043047017055
Amadeus <amadeeus@bk.ru>
2026-05-02 04:35:49 +0300

    dev(add): Добавлены файлы миграций и readme

---
+++ FinanceService.Infrastructure/Migrations/20260430144031_Init.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CurrencyRate",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Rate = table.Column<decimal>(type: "numeric", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyRate", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyRate_Name",
+                table: "CurrencyRate",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrencyRate");
+        }
+    }
+}
